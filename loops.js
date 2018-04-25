33a0e866caa4ca42b3950594661c248e9b6f5d08
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-25 23:01:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,10 +17,14 @@ function whileLoop(number) {
   return 'done'
 }
 
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array) {
   do {
     
-  } while {
-    
-  }
+  } while 
+    (maybeTrue());
 }
