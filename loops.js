45ab38fc0a419789193c072737bb2a6d02556b00
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-25 22:57:18 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop")
+    } else {
+      array.push("I am ${i} strane loops.")
+    }
+  }
+  return array
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(number)
+    number -= 1
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  
+}
