4264cc7fb332b16ccba821db8f6a93366894532d
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-25 23:12:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,7 +24,8 @@ function maybeTrue() {
 
 function doWhileLoop(array) {
   do {
-    array.
+    array.pop()
   } while 
-    (maybeTrue());
+    (array.length > 0 || maybeTrue())
+    return array
 }
